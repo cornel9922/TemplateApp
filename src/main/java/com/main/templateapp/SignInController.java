5d2f447121a2e31f3299c2ce98f537980e6d16e3
Cornel <cornel.cornel200@gmail.com>
2022-08-21 18:07:35 +0300

    app v0.3

---
+++ src/main/java/com/main/templateapp/SignInController.java
@@ -0,0 +1,20 @@
+package com.main.templateapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class SignInController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
